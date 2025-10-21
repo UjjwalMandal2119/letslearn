4f3f183af7e7c2fc809f010099a7fce2eb907fb8
UjjwalMandal2119 <157363081+UjjwalMandal2119@users.noreply.github.com>
2025-10-21 16:17:22 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,125 @@
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
-
-function addTask() {
-    if (inputBox.value === '') {
-        alert("You must write something!");
-    } else {
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML="\u00d7";
-        li.appendChild(span);
+const questions= [
+    {
+        question:"Which is the largest country?",
+        answers:[
+            {text: "Sweeden", correct:  false},
+            {text: "Spain", correct:  false},
+            {text: "Russia", correct:  true},
+            {text: "India", correct:  false},
+        ]
+    },
+    {
+        question:"Which is the largest animal?",
+        answers:[
+            {text: "Shark", correct:  false},
+            {text: "Blue whale", correct:  true},
+            {text: "Elephant", correct:  false},
+            {text: "Giraffe", correct:  false},
+        ]
+    },
+    {
+        question:"Which is the smallest continent in the world?",
+        answers:[
+            {text: "Asia", correct:  false},
+            {text: "Australia", correct:true},
+            {text: "Arctic", correct:  false},
+            {text: "Africa", correct:  false},
+        ]
+    },
+    {
+        question:"Which is the largest largest DEsert in the world?",
+        answers:[
+            {text: "Thar", correct:  false},
+            {text: "Runn of Kutch", correct:  false},
+            {text: "Sahara", correct:  true},
+            {text: "Antarctica", correct:  false},
+        ]
     }
-    inputBox.value="";
-    saveData();
-}
- addTask();
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName=== "Li"){
-        e.target.classList.toggle("checked");
-        saveData();
-    }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        saveData();
-    }
-},false);
 
-function saveData(){
-  localStorage.setItem("data", listContainer.innerHTML);
+
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex=0;
+let score=0;
+
+function startQuiz(){
+    currentQuestionIndex=0;
+    score=0;
+    nextButton.innerHTML="Next";
+    showQuestion();
 }
 
-function showTask(){
-    listContainer.innerHTML = localStorage.getItem("data");
+function showQuestion(){
+    resetState();
+    let currentQuestion= questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex +1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
 }
-showTask();
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+
+function selectAnswer(e){
+    const selectedBtn=e.target;
+    const isCorrect = selectedBtn.dataset.correct==="true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button =>{
+         if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+         }
+         button.disabled= true;
+    });
+    nextButton.style.display = "block";
+}
+
+function  showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML="play Again";
+    nextButton.style.display="block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex<questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex<questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+});
+
+
+startQuiz();
